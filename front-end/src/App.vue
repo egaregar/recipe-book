4809ecb3610d05342aae81ebc228d15954888924
egaregar <plane47@gmail.com>
2020-12-17 20:03:39 -1000

    Test

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,32 +1,87 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+<div id="app">
+  <div class="header">
+    <router-link to="/">
+      <div class="logo">
+        <img src="/ordinary.png">
+      </div>
+    </router-link>
+    <div class="title">
+      <h1>Museum of Ordinary Objects</h1>
     </div>
-    <router-view/>
   </div>
+  <div class="content">
+    <router-view />
+  </div>
+  <div class="footer">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/admin">Admin</router-link>
+    <p class="name_footer"> Ethan Garlick; <a href="https://github.com/BYU-CS-260-Winter-2020/museum-of-ordinary-objects.git">Git Hub</a> </p>
+  </div>
+</div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+html {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+  background: #fff;
+  padding: 0px;
+  margin: 0px;
+}
+
+/* Header */
+.header {
+  display: flex;
+  padding: 10px 100px 0px 100px;
+  background-color: #5BDEFF;
+  color: #1C454F;
+}
+
+.title {
+  margin-top: 5px;
+}
+
+.title h1 {
+  font-size: 30px;
+}
+
+.content {
+  padding: 20px 100px;
+  min-height: 500px;
+}
+
+/* Footer */
+.footer {
+  height: 50px;
+  padding: 20px 100px 0px 100px;
+  background: #e3e3e3;
+  font-size: 12px;
+}
+
+.footer a {
+  color: #000;
+}
+
+.name_footer {
+  font-size: 12px;
   text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+a:hover,
+a:visited:hover {
+  text-decoration: none;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+h1 {
+  font-size: 20px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+h2 {
+  font-size: 14px;
 }
 </style>
